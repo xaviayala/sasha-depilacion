79f0e815282ed4abeee118c82cb6417fd6e5952f
F. Xavier Ayala <xavi.ayala@gmail.com>
2017-03-03 14:37:46 +0100

    Publishing to gh-pages (publish.sh)

--- deploy/publish_toghpages.sh
+++ deploy/publish_toghpages.sh
@@ -21,7 +21,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo --theme=hugo-creative-portfolio-theme --buildDrafts
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
